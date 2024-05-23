20c0716724b03dfd7724d42cae5991d9c59cf766
0xcuonghx <cuonghx@gu-tech.com>
2024-05-23 12:20:19 +0700

    [rs] minigrep

---
+++ rust/enums/src/main.rs
@@ -0,0 +1,24 @@
+fn main() {
+    enum Message {
+        Quit,
+        Move { x: i32, y: i32 },
+        Write(String),
+        ChangeColor(i32, i32, i32),
+    }
+    struct QuitMessage; // unit struct
+    struct MoveMessage {
+        x: i32,
+        y: i32,
+    }
+    struct WriteMessage(String); // tuple struct
+    struct ChangeColorMessage(i32, i32, i32); // tuple struct
+
+    impl Message {
+        fn call(&self) {
+            // method body would be defined here
+        }
+    }
+
+    let m = Message::Write(String::from("hello"));
+    m.call();
+}
