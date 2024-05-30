1433ad6a8211fa25b2519edbdce6190bb7acc872
0xcuonghx <cuonghx@gu-tech.com>
2024-05-30 08:35:07 +0700

    [ton] 5_first_contract

---
+++ ton/5_first_contract/scripts/deploy.ts
@@ -0,0 +1,53 @@
+import {
+  Cell,
+  StateInit,
+  beginCell,
+  contractAddress,
+  storeStateInit,
+  toNano,
+} from "@ton/core";
+import { hex } from "../build/main.compiled.json";
+import qs from "qs";
+import qrcode from "qrcode-terminal";
+
+async function deployScript() {
+  console.log(
+    "================================================================="
+  );
+  console.log("Deploy script is running, let's deploy our main.fc contract...");
+
+  const codeCell = Cell.fromBoc(Buffer.from(hex, "hex"))[0];
+  const dataCell = new Cell();
+
+  const stateInit: StateInit = {
+    code: codeCell,
+    data: dataCell,
+  };
+
+  const stateInitBuilder = beginCell();
+  storeStateInit(stateInit)(stateInitBuilder);
+  const stateInitCell = stateInitBuilder.endCell();
+
+  const address = contractAddress(0, {
+    code: codeCell,
+    data: dataCell,
+  });
+
+  let link =
+    `https://tonhub.com/transfer/` +
+    address.toString({
+      testOnly: true,
+    }) +
+    "?" +
+    qs.stringify({
+      text: "Deploy contract",
+      amount: toNano(1).toString(10),
+      init: stateInitCell.toBoc({ idx: false }).toString("base64"),
+    });
+
+  qrcode.generate(link, { small: true }, (code) => {
+    console.log(code);
+  });
+}
+
+deployScript();
